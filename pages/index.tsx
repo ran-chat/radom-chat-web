91b9577f0c487e680940ad77411279e9bbd9f619
hanamDeveloper <zmf1118@gmail.com>
2023-05-04 14:55:29 +0900

    아아악1

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
       <div>
         <h1>랜덤 채팅 앱 입니다.</h1>
         <TestButton>
-          테스트 버튼 허스키 적용dddddddㅇㅇㅇㄴㅇddㅇsddㄴㅇdㅇ
+          테스트 버튼 허스키 적용dddddddㅇㅇㅇㄴㅇddㅇsddㄴㅇdㅇd
         </TestButton>
         <Login />
       </div>
